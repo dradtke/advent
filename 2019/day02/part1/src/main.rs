a45faec3094ae7cf21d236df31e5d3d8ad5a821c
Damien Radtke <damienradtke@gmail.com>
2019-12-02 11:41:03 -0600

    Finish Day 2 part 1

--- 2019/day02/part1/src/main.rs
+++ 2019/day02/part1/src/main.rs
@@ -2,20 +2,68 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() {
-    let iter = io::stdin().lock().split(b',');
+    let mut program = read_program(io::stdin().lock());
+    program[1] = 12;
+    program[2] = 2;
+    program = run(program);
+    println!("Result: {}", program[0]);
+}
+
+type Program = Vec<usize>;
+
+fn read_program<R: BufRead>(input: R) -> Program {
+    input.split(b',')
+        .map(|result| result.unwrap())
+        .map(|bytes| String::from_utf8(bytes).unwrap())
+        .map(|string| string.trim().parse::<usize>().unwrap())
+        .collect()
+}
+
+fn run(mut program: Program) -> Program {
+    let mut i = 0;
     loop {
-        match (iter.next()) {
-            Some(Ok(vec![b'1'])) => {}, // add
-            Some(Ok(vec![b'2'])) => {}, // multiply
-            Some("99") => break, // exit
-            Some(x) => panic!("unexpected opcode: {}", x),
-            None => panic!("unexpected EOF"),
+        match program[i] {
+            1 => {
+                let input1 = program[i+1];
+                let input2 = program[i+2];
+                let output = program[i+3];
+                program[output] = program[input1] + program[input2];
+                i += 4;
+            },
+            2 => {
+                let input1 = program[i+1];
+                let input2 = program[i+2];
+                let output = program[i+3];
+                program[output] = program[input1] * program[input2];
+                i += 4;
+            },
+            99 => break,
+            x => panic!("unexpected opcode: {}", x),
         }
     }
+    program
 }
 
-fn add(input1: i32, input2: i32, output: i32) {
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-fn multiply(input1: i32, input2: i32, output: i32) {
+    #[test]
+    fn test_read_program() {
+        assert_eq!(read_program("1,2,3,4".as_bytes()), vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_read_program_with_newline() {
+        assert_eq!(read_program("1,2,3,4\n".as_bytes()), vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_run() {
+        assert_eq!(run(vec![1,9,10,3,2,3,11,0,99,30,40,50]), vec![3500,9,10,70,2,3,11,0,99,30,40,50]);
+        assert_eq!(run(vec![1,0,0,0,99]), vec![2,0,0,0,99]);
+        assert_eq!(run(vec![2,3,0,3,99]), vec![2,3,0,6,99]);
+        assert_eq!(run(vec![2,4,4,5,99,0]), vec![2,4,4,5,99,9801]);
+        assert_eq!(run(vec![1,1,1,4,99,5,6,0,99]), vec![30,1,1,4,2,5,6,0,99]);
+    }
 }
