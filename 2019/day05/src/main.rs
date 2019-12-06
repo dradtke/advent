b09d037cf79c250e14493ef8c031e0be96a309d7
Damien Radtke <damienradtke@gmail.com>
2019-12-05 19:07:32 -0500

    Add Day 5 part 2

--- 2019/day05/src/main.rs
+++ 2019/day05/src/main.rs
@@ -1,6 +1,6 @@
+use std::convert::TryInto;
 use std::io;
 use std::io::prelude::*;
-use std::sync::mpsc::Sender;
 
 const TEN: isize = 10;
 
@@ -24,7 +24,11 @@ impl Mode {
 
 fn main() {
     let program = read_program(io::stdin().lock());
-    run(program);
+    println!("part 1:");
+    run(program.clone(), 1);
+    println!("");
+    println!("part 2:");
+    run(program.clone(), 5);
 }
 
 fn read_program<R: BufRead>(input: R) -> Program {
@@ -35,19 +39,11 @@ fn read_program<R: BufRead>(input: R) -> Program {
         .collect()
 }
 
-fn read_program_args(program: &Program, modes: isize, i: &mut usize, n: usize) -> Vec<isize> {
+fn read_program_args(program: &Program, i: &mut usize, n: usize) -> Vec<isize> {
     let start = *i+1;
     let end = start+n;
     *i = end;
     program[start..end].to_vec()
-        /*
-        .enumerate()
-        .map(|(i, param)| {
-            let mode = Mode::from((modes / ten.pow(i as u32)) % ten);
-            get_param_value(&program, mode, *param)
-        })
-        .collect()
-        */
 }
 
 fn get_param_value(program: &Program, args: &Vec<isize>, modes: isize, index: usize) -> isize {
@@ -59,7 +55,8 @@ fn get_param_value(program: &Program, args: &Vec<isize>, modes: isize, index: us
     }
 }
 
-fn run(mut program: Program) -> Program {
+// TODO: figure out a better way to capture the output
+fn run(mut program: Program, input: isize) -> Program {
     let mut i = 0;
     loop {
         let opcode = program[i] % 100; // last two digits
@@ -67,14 +64,14 @@ fn run(mut program: Program) -> Program {
         // println!("processing: {} (opcode = {}, modes = {})", program[i], opcode, modes);
         match opcode {
             1 => {
-                let args = read_program_args(&program, modes, &mut i, 3);
+                let args = read_program_args(&program, &mut i, 3);
                 let param1 = get_param_value(&program, &args, modes, 0);
                 let param2 = get_param_value(&program, &args, modes, 1);
                 // println!("adding {} + {} -> {} saved to {}", param1, param2, param1 + param2, args[2]);
                 program[args[2] as usize] = param1 + param2;
             },
             2 => {
-                let args = read_program_args(&program, modes, &mut i, 3);
+                let args = read_program_args(&program, &mut i, 3);
                 let param1 = get_param_value(&program, &args, modes, 0);
                 let param2 = get_param_value(&program, &args, modes, 1);
                 // println!("multing {} * {} -> {} saved to {}", param1, param2, param1 * param2, args[2]);
@@ -82,16 +79,49 @@ fn run(mut program: Program) -> Program {
             },
             3 => {
                 // input
-                let args = read_program_args(&program, modes, &mut i, 1);
-                let input = 1;
+                let args = read_program_args(&program, &mut i, 1);
                 // println!("inputting value to {}: {}", args[0], input);
                 program[args[0] as usize] = input;
             },
             4 => {
                 // output
-                let args = read_program_args(&program, modes, &mut i, 1);
+                let args = read_program_args(&program, &mut i, 1);
                 let output = get_param_value(&program, &args, modes, 0);
                 println!("output: {}", output);
+            },
+            5 => {
+                // jump-if-true
+                let args = read_program_args(&program, &mut i, 2);
+                let param1 = get_param_value(&program, &args, modes, 0);
+                let param2 = get_param_value(&program, &args, modes, 1);
+                if param1 != 0 {
+                    i = param2.try_into().unwrap();
+                }
+            },
+            6 => {
+                // jump-if-false
+                let args = read_program_args(&program, &mut i, 2);
+                let param1 = get_param_value(&program, &args, modes, 0);
+                let param2 = get_param_value(&program, &args, modes, 1);
+                if param1 == 0 {
+                    i = param2.try_into().unwrap();
+                }
+            },
+            7 => {
+                // less-than
+                let args = read_program_args(&program, &mut i, 3);
+                let param1 = get_param_value(&program, &args, modes, 0);
+                let param2 = get_param_value(&program, &args, modes, 1);
+                let dest = args[2];
+                program[dest as usize] = if param1 < param2 { 1 } else { 0 };
+            },
+            8 => {
+                // equals
+                let args = read_program_args(&program, &mut i, 3);
+                let param1 = get_param_value(&program, &args, modes, 0);
+                let param2 = get_param_value(&program, &args, modes, 1);
+                let dest = args[2];
+                program[dest as usize] = if param1 == param2 { 1 } else { 0 };
             },
             99 => break,
             x => panic!("unexpected opcode: {}", x),
@@ -103,17 +133,4 @@ fn run(mut program: Program) -> Program {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_run() {
-        assert_eq!(run(vec![1,9,10,3,2,3,11,0,99,30,40,50]), vec![3500,9,10,70,2,3,11,0,99,30,40,50]);
-        assert_eq!(run(vec![1,0,0,0,99]), vec![2,0,0,0,99]);
-        assert_eq!(run(vec![2,3,0,3,99]), vec![2,3,0,6,99]);
-        assert_eq!(run(vec![2,4,4,5,99,0]), vec![2,4,4,5,99,9801]);
-        assert_eq!(run(vec![1,1,1,4,99,5,6,0,99]), vec![30,1,1,4,2,5,6,0,99]);
-
-        // new ones
-        assert_eq!(run(vec![1002,4,3,4,33]), vec![1002,4,3,4,99]);
-        assert_eq!(run(vec![1101,100,-1,4,0]), vec![1101,100,-1,4,99]);
-    }
 }
