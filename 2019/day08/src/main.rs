e8d54b592d5cbb5f6a842bbbbb55a17e4ccae1a2
Damien Radtke <git@damienradtke.com>
2019-12-15 09:07:44 -0600

    Day 8 part 1

---
+++ 2019/day08/src/main.rs
@@ -0,0 +1,88 @@
+use std::io::prelude::*;
+
+type Layer = Vec<Vec<u8>>;
+type Image = Vec<Layer>;
+
+fn main() -> std::io::Result<()> {
+    let mut input = String::new();
+    std::io::stdin().lock().read_to_string(&mut input)?;
+
+    let image = parse_image(25, 6, input.chars().flat_map(parse_char));
+    let fewest_zeros = image.iter()
+        .map(|layer| count_digits(layer, 0))
+        .enumerate()
+        .min_by_key(|(_, total)| *total)
+        .unwrap().0;
+
+    println!("part 1: {}", count_digits(&image[fewest_zeros], 1) * count_digits(&image[fewest_zeros], 2));
+
+    Ok(())
+}
+
+fn parse_image<I: Iterator<Item=u8>>(width: usize, height: usize, data: I) -> Image {
+    let pixels_per_layer = width*height;
+
+    let mut image = Vec::new();
+    for (i, pixel) in data.into_iter().enumerate() {
+        let layer_index = i/pixels_per_layer;
+        if layer_index == image.len() {
+            let layer: Layer = (0..height).map(|_| new_row(width)).collect();
+            image.push(layer);
+        }
+        let pixel_index = i%pixels_per_layer;
+        let pixel_y = pixel_index/width;
+        let pixel_x = pixel_index%width;
+        image[layer_index][pixel_y][pixel_x] = pixel;
+    }
+    image
+}
+
+fn new_row(width: usize) -> Vec<u8> {
+    let mut row = Vec::with_capacity(width);
+    row.resize(width, 0);
+    row
+}
+
+fn parse_char(c: char) -> Option<u8> {
+    c.to_digit(10).map(|x| x as u8)
+}
+
+fn count_digits(layer: &Layer, digit: u8) -> usize {
+    layer.iter().fold(0, |acc, row| acc + row.iter().filter(|pixel| **pixel == digit).count())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_image() {
+        let data = vec![1,2,3,4,5,6,7,8,9,0,1,2].into_iter();
+        assert_eq!(parse_image(3, 2, data), vec![
+            [[1,2,3],[4,5,6]],
+            [[7,8,9],[0,1,2]],
+        ]);
+    }
+
+    #[test]
+    fn test_count_digits() {
+        let layer = vec![vec![1,1,1,3],vec![2,2,3,3]];
+        assert_eq!(count_digits(&layer, 1), 3);
+        assert_eq!(count_digits(&layer, 2), 2);
+        assert_eq!(count_digits(&layer, 3), 3);
+    }
+
+    #[test]
+    fn test_parse_char() {
+        assert_eq!(parse_char('0'), Some(0));
+        assert_eq!(parse_char('1'), Some(1));
+        assert_eq!(parse_char('2'), Some(2));
+        assert_eq!(parse_char('3'), Some(3));
+        assert_eq!(parse_char('4'), Some(4));
+        assert_eq!(parse_char('5'), Some(5));
+        assert_eq!(parse_char('6'), Some(6));
+        assert_eq!(parse_char('7'), Some(7));
+        assert_eq!(parse_char('8'), Some(8));
+        assert_eq!(parse_char('9'), Some(9));
+    }
+}
