7054c1bcf12d3398b58d96388541c36bc68481dc
Damien Radtke <git@damienradtke.com>
2020-12-05 15:09:25 -0600

    Extract common 2020 Day 5 logic to new module

--- 2020/day05/part1.erl
+++ 2020/day05/part1.erl
@@ -1,29 +1,7 @@
 -module(part1).
--export([start/0, lower/1, upper/1, locate/2, find_seat/1, seat_id/1]).
-
-input() -> case io:get_line("") of
-			   eof -> [];
-			   {error, Desc} -> erlang:error(Desc);
-			   Line -> [string:trim(Line)] ++ input()
-		   end.
-
-lower({A, B}) -> {A, A + ((B-A) div 2)}.
-upper({A, B}) -> {A + ((B-A) div 2) + 1, B}.
-
-locate([], {A, _}) -> A;
-locate([$F|Tail], Range) -> locate(Tail, lower(Range));
-locate([$L|Tail], Range) -> locate(Tail, lower(Range));
-locate([$B|Tail], Range) -> locate(Tail, upper(Range));
-locate([$R|Tail], Range) -> locate(Tail, upper(Range)).
-
-find_seat(Line) -> {
-			locate(lists:sublist(Line, 1, 7), {0, 127}),
-			locate(lists:sublist(Line, 8, 3), {0, 7})
-		   }.
-
-seat_id({Row, Column}) -> (Row * 8) + Column.
+-export([start/0]).
 
 start() ->
-	Seats = lists:map(fun find_seat/1, input()),
-	SeatIds = lists:map(fun seat_id/1, Seats),
+	Seats = lists:map(fun day05:find_seat/1, day05:input()),
+	SeatIds = lists:map(fun day05:seat_id/1, Seats),
 	io:format("Result: ~p~n", [lists:max(SeatIds)]).
