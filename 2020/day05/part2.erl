53596946a7c55e485979330e0644bfdc64a4dbf7
Damien Radtke <git@damienradtke.com>
2020-12-05 14:57:13 -0600

    2020 Day 5

---
+++ 2020/day05/part2.erl
@@ -0,0 +1,30 @@
+-module(part2).
+-export([start/0, lower/1, upper/1, locate/2, find_seat/1, seat_id/1]).
+
+input() -> case io:get_line("") of
+			   eof -> [];
+			   {error, Desc} -> erlang:error(Desc);
+			   Line -> [string:trim(Line)] ++ input()
+		   end.
+
+lower({A, B}) -> {A, A + ((B-A) div 2)}.
+upper({A, B}) -> {A + ((B-A) div 2) + 1, B}.
+
+locate([], {A, _}) -> A;
+locate([$F|Tail], Range) -> locate(Tail, lower(Range));
+locate([$L|Tail], Range) -> locate(Tail, lower(Range));
+locate([$B|Tail], Range) -> locate(Tail, upper(Range));
+locate([$R|Tail], Range) -> locate(Tail, upper(Range)).
+
+find_seat(Line) -> {
+			locate(lists:sublist(Line, 1, 7), {0, 127}),
+			locate(lists:sublist(Line, 8, 3), {0, 7})
+		   }.
+
+seat_id({Row, Column}) -> (Row * 8) + Column.
+
+start() ->
+	Seats = lists:map(fun find_seat/1, input()),
+	SeatIds = lists:map(fun seat_id/1, Seats),
+	PossibleSeats = lists:seq(lists:min(SeatIds), lists:max(SeatIds)),
+	io:format("Result: ~p~n", [hd(PossibleSeats -- SeatIds)]).
